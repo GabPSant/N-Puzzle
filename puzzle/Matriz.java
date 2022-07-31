f7847b454a7d07702f69f3201520b2e309b7b1be
Gabriel P. Santos <gabrielp_santos@hotmail.com>
2022-07-30 23:35:35 -0300

    testes

--- puzzle/Matriz.java
+++ puzzle/Matriz.java
@@ -2,22 +2,21 @@ package puzzle;
 import java.util.*;
 
 public class Matriz {
-
+    
     private int pecas[][] = new Array [2][2];
 
-    public int Tabela(int x, int y){
+    public void Tabela(int x, int y){
         Random gerador = new Random();
-        for(x = 0; x<2; x++){
-            for(y=0; y<2; y++){
+        for(x = 0; x<3; x++){
+            for(y=0; y<3; y++){
                 pecas[x][y] = gerador.nextInt(9);
                 if(pecas[x][y]==0){pecas[x][y] = null;}
             }
             System.out.println(pecas[x][y]);
         }
-        return x;
     }
     public static void main(String[] args){
-        Matriz estudo = new Matriz();
-        estudo.Tabela(0,0);
+        Matriz processo = new Matriz();
+        processo.Tabela(0, 0);
     }
 }
