87f289d48db25d7b61efa6e29d75d432c6f9792f
denilsonbomjesus <denilsonjose.bomjesus@gmail.com>
2022-08-01 01:34:46 -0300

    limitando de 1 a 8

--- puzzle/Teste1.java
+++ puzzle/Teste1.java
@@ -3,23 +3,32 @@ package puzzle;
 import java.util.Arrays;
 import java.util.Random;
 
+
 public class Teste1 {
 
-
 	public static void main(String[] args) {
+		
+		boolean iguais = false;
 		 
 		int[][] pecas = new int[3][3];
 		
-	     Random gerador = new Random();
-	     int x, y;
-	        for(x = 0; x<3; x++){
-	            for(y=0; y<3; y++){
-	                pecas[x][y] = gerador.nextInt(9);
-	                if(pecas[x][y]==0){
-	                	pecas[x][y] = 0;}
+	    Random gerador = new Random();
+	    int x, y;
+	       for(x = 0; x<3; x++){
+	           for(y=0; y<3; y++){
+	               pecas[x][y] = gerador.nextInt(8) + 1;
+	               if(pecas[x][y]==0){
+	                 pecas[x][y] = 0;
+	                	}
+	               if (x != y) {
+	                 if (pecas[x] == pecas[y]) {
+	                		iguais = true;
+	                	}
+	                }
 	                }
 	            }
 		System.out.print(Arrays.deepToString(pecas));
 	}
+
 
 }
