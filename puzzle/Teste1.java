dd4084c35b0a9697b1f9cb1855aebdceb32cb0e3
denilsonbomjesus <denilsonjose.bomjesus@gmail.com>
2022-08-01 23:16:48 -0300

    Troca de Random por Collections; Numeros sem repetição

--- puzzle/Teste1.java
+++ puzzle/Teste1.java
@@ -1,32 +1,36 @@
 package puzzle;
 
 import java.util.Arrays;
-import java.util.Random;
+import java.util.ArrayList; 
+import java.util.Collections; 
 
 
 public class Teste1 {
 
 	public static void main(String[] args) {
 		
-		boolean iguais = false;
-		 
+	    int x, y, n, m = 0;   
+		
+	    ArrayList<Integer> numeros = new ArrayList<Integer>(); 
 		int[][] pecas = new int[3][3];
 		
-	    Random gerador = new Random();
-	    int x, y;
-	       for(x = 0; x<3; x++){
-	           for(y=0; y<3; y++){
-	               pecas[x][y] = gerador.nextInt(8) + 1;
-	               if(pecas[x][y]==0){
-	                 pecas[x][y] = 0;
-	                	}
-	               if (x != y) {
-	                 if (pecas[x] == pecas[y]) {
-	                		iguais = true;
+		for(n = 0; n < 9; n++){ 
+			   numeros.add(n); 
+			  } 
+		
+		Collections.shuffle(numeros); 
+			  
+	       for(x = 0; x < 3; x++){
+	           for(y = 0; y < 3; y++){
+	        	   pecas[x][y] = numeros.get(m); 
+	    	       m++; 	               
+
+	    	       if(pecas[x][y] == 0){  
+		                  pecas[x][y] =  x; 
 	                	}
 	                }
 	                }
-	            }
+	            
 		System.out.print(Arrays.deepToString(pecas));
 	}
 
