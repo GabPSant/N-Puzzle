8e03f166d6233d64e7c38a21f3a515970a00999e
Gabriel P. Santos <gabrielp_santos@hotmail.com>
2022-08-11 14:47:47 -0300

    Mudança

--- puzzle/Base.java
+++ puzzle/Base.java
@@ -1,17 +1,17 @@
 package puzzle;
 
 import java.util.Arrays;
+
 import java.util.ArrayList; 
 import java.util.Collections; 
 
-
-public class Teste1 {
+public class Base {
 	private int x;
     private int y;
 	private int pecas[][] = new int[3][3];
 	private int m;
 	// definir é o valor que define o  tamanho e quantidade de valores dentro da matriz
-	public String Base(int definir) {
+	public String Calculo(int definir) {
 		ArrayList<Integer> numeros = new ArrayList<Integer>();
 		m = 0;
 		for(int n = 0; n < (definir*definir); n++){// Cria o conjunto de números usados na Matriz
@@ -21,17 +21,15 @@ public class Teste1 {
 		//System.out.println(numeros);
 	       for(x = 0; x < definir; x++){ // Defini cada espaço do array que forma a matriz
 	           for(y = 0; y < definir; y++){
-				   
 	               pecas[x][y] = numeros.get(m);
 				   m++;
-	               if(pecas[x][y] == 0){pecas[x][y] =  x;}
 	            }
 	        }
 		System.out.print(Arrays.deepToString(pecas));
 		return Arrays.deepToString(pecas);
 	}
 		public static void main(String[] args){
-		Teste1 calculo = new Teste1();
-		calculo.Base(3);
+		Base calculo = new Base();
+		calculo.Calculo(3);
 	}
 }
