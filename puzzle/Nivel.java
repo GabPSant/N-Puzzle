c759233f9e6e9f6d6db16cf30444e3695b478e3c
denilsonbomjesus <denilsonjose.bomjesus@gmail.com>
2022-08-15 22:15:33 -0300

    mudança nos níveis

--- puzzle/Nivel.java
+++ puzzle/Nivel.java
@@ -1,23 +1,29 @@
 package puzzle;
 
 public class Nivel extends Base{
-	private String nivel = "Dificil";
-	public void Dificuldade() {
+	
+	public void Dificuldade(String nivel) {
+		
 		if(nivel == "Facil") {
 			Base calculo = new Base();
 			calculo.Calculo(3);
 		}
+		
 		else if(nivel == "Medio") {
 			Base calculo = new Base();
 			calculo.Calculo(4);
 		}
+		
 		else if(nivel == "Dificil") {
 			Base calculo = new Base();
 			calculo.Calculo(5);
 		}
 	}
+	
 	public static void main(String[] args) {
+		
 		Nivel teste = new Nivel();
-		teste.Dificuldade();
+		//testando
+		teste.Dificuldade("Medio");
 	}
 }
