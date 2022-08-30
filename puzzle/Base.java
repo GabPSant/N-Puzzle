8fdcaa59288a3add6fdbcbbda38f455cb4faefa4
denilsonbomjesus <denilsonjose.bomjesus@gmail.com>
2022-08-29 23:36:59 -0300

    criação de construtor para letras

--- puzzle/Base.java
+++ puzzle/Base.java
@@ -23,14 +23,13 @@ public class Base {
 	public int[][] Calculo() {
 		pecas = new int[definir][definir];
 		ArrayList<Integer> numeros = new ArrayList<Integer>();
-		char numeral = 'a';
-		for(int n = numeral; n < (definir*definir); n++){// Cria o conjunto de números usados na Matriz
+		for(int n = 0; n < (definir*definir); n++){// Cria o conjunto de números usados na Matriz
 			numeros.add(n);
 		}
 		Collections.shuffle(numeros);//Embaralha os numeros de forma aleátoria
 		//System.out.println(numeros);
-	       for(x = numeral; x < definir; x++){ // Defini cada espaço do array que forma a matriz
-	           for(y = numeral; y < definir; y++){
+	       for(x = 0; x < definir; x++){ // Defini cada espaço do array que forma a matriz
+	           for(y = 0; y < definir; y++){
 	               pecas[x][y] = numeros.get(m);
 				   m++;
 	            }
